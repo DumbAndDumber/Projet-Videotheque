40a3879563590ef3ac76edb1f8ca4e2f092c3b9a
Splatatozor <kabbaj.samuel@hotmail.com>
2017-01-12 16:39:10 +0100

    Add card series

--- videotheque/src/app/app.module.ts
+++ videotheque/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { Storage } from '@ionic/storage';
 import { MyApp } from './app.component';
 import { FilmsPage } from '../pages/films/films';
 import { SeriesPage } from '../pages/series/series';
@@ -45,6 +46,6 @@ import { User } from '../classes/user';
     SignUpPage,
     LoginPage,
   ],
-  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Api, User]
+  providers: [{provide: ErrorHandler, useClass: IonicErrorHandler}, Api, User, Storage]
 })
 export class AppModule {}
