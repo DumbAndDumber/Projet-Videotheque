f403d2b5206ec94a3617527350d983930c3d0d23
splatatozor <jenkins@sam-PC>
2017-05-12 11:06:12 +0200

    Fix parameters

--- videotheque/src/scripts/parameters.php
+++ videotheque/src/scripts/parameters.php
@@ -1,7 +1,7 @@
 <?php
 	header('Access-Control-Allow-Origin: *');
 
-	$arrestDbUrl = "http://www.sarryromain.com/videotheque/arrestdb.php";
+	$arrestDbUrl = "http://www.sarryromain.com/videotheque/arrestdb.php/";
 	//$arrestDbUrl = "http://localhost/projets/Projet-Videotheque/database/arrestdb.php/";
 	$omdbApiUrl =  "http://www.omdbapi.com/";
 ?>
